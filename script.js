32fbe185c60d749e6de3233ef6ed3ed1a0df387e
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2024-07-25 15:07:11 -0300

    f

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-// script.js
+const buttons = document.querySelectorAll(".button");
+const infoImage = document.getElementById("info-image");
+const infoText = document.getElementById("info-text");
 
-const botoes = document.querySelectorAll(".step");
-const textoInfo = document.getElementById("info-text");
-const imagemInfo = document.getElementById("info-image");
+const images = {
+    plantio: "img/plantio.jpeg",
+    crescimento: "img/crescimento.jpeg",
+    colheita: "img/colheita.jpeg",
+    processamento: "img/processamento.jpeg",
+    distribuicao: "img/distribuicao.jpeg"
+};
 
-const imagens = [
-    "img/plantio.jpeg",
-    "img/crescimento.jpeg", // Substitua com o nome da imagem para crescimento
-    "img/colheita.jpeg",
-    "img/processamento.jpeg", // Substitua com o nome da imagem para processamento
-    "img/distribuicao.jpeg" // Substitua com o nome da imagem para distribuição
-];
+const texts = {
+    plantio: "Texto informativo sobre o plantio.",
+    crescimento: "Texto informativo sobre o crescimento das plantas.",
+    colheita: "Texto informativo sobre a colheita.",
+    processamento: "Texto informativo sobre o processamento das plantas.",
+    distribuicao: "Texto informativo sobre a distribuição das plantas."
+};
 
-const textos = [
-    "O plantio é a primeira etapa do ciclo alimentar, onde as sementes são colocadas no solo e começam a germinar.",
-    "Durante o crescimento, as plantas se desenvolvem e se fortalecem, absorvendo nutrientes e água do solo.",
-    "A colheita ocorre quando os alimentos atingem o ponto ideal de maturação e estão prontos para serem recolhidos.",
-    "O processamento envolve a transformação dos alimentos colhidos em produtos finais prontos para consumo.",
-    "A distribuição é a etapa final, onde os alimentos são entregues aos consumidores através de diversos canais de venda."
-];
-
-botoes.forEach((botao, index) => {
-    botao.addEventListener('click', () => {
-        // Remove a classe 'ativo' de todos os botões
-        botoes.forEach(b => b.classList.remove("ativo"));
-        
-        // Adiciona a classe 'ativo' ao botão clicado
-        botao.classList.add("ativo");
-        
-        // Atualiza a imagem e o texto informativo
-        imagemInfo.src = imagens[index];
-        textoInfo.textContent = textos[index];
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const infoKey = button.getAttribute("data-info");
+        infoImage.src = images[infoKey];
+        infoText.textContent = texts[infoKey];
     });
 });
